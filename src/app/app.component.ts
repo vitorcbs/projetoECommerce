eb646f1fd4d491f1d48e7d7509a8ab8539d845c2
Vitinho <vitorcbs18@gmail.com>
2024-06-06 23:48:06 -0300

    feat: :lipstick: Implement footer and add to app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClient, HttpClientModule, HttpHandler } from '@angular/common/http';
 import { HeaderComponent } from './modules/layout/header/header.component';
+import { FooterComponent } from './modules/layout/footer/footer.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, HttpClientModule, HeaderComponent],
+  imports: [NxWelcomeComponent, RouterModule, HttpClientModule, HeaderComponent, FooterComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
